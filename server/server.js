d42434e38a9e526b095799e6a9a062d45d290d60
rykun <rykunek@hotmail.com>
2017-08-06 23:50:02 +0200

    add get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res)=>{
     })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos)=>{
+    res.status(200).send({todos});
+  }, (e)=>{
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, ()=>{
   console.log('Server started at port 3000');
 })

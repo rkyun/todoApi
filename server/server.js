eff7de42d9991e3739a84d23455e80859b3e76c4
Michał <rykunek@hotmail.com>
2017-08-06 19:03:36 +0200

    refactoring, post todo route

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser =  require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=>{
+    console.log(req.body);
+    var todo = new Todo({
+      text: req.body.text
+    })
+    todo.save().then((doc)=>{
+      res.status(200).send(doc);
+    }, (e)=>{
+      res.status(400).send(e);
+    })
+});
+
+app.listen(3000, ()=>{
+  console.log('Server started at port 3000');
+})

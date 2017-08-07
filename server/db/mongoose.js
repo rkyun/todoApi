ef937801089076c4cef519c865ec7f176880a33c
rykun <rykunek@hotmail.com>
2017-08-07 23:13:21 +0200

    heroku + mongodb mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true});
+//localhost
+// mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true});
+
+//heroku
+mongoose.connect('mongodb://rkn:123@ds137191.mlab.com:37191/todoapp', { useMongoClient: true});
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} =  require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const port = process.env.PORT || 3000;
 
 var app=express();
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) =>{
   });
 });
 
-app.listen(3000, ()=>{
-  console.log('Server started at port 3000');
+app.listen(port, ()=>{
+  console.log(`Server started at port ${port}`);
 })
 
 module.exports = { app };

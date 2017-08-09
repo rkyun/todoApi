1c0c857cda10ab46db2e5b56c82554de766881a4
rykun <rykunek@hotmail.com>
2017-08-09 23:49:04 +0200

    prod db fix

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if(env==='development'){
 } else if(env==='test'){
     process.env.MONGODB_URI = 'mongodb://localhost:27017/toDoAppTest';
     process.env.PORT = 3000;
+} else if (env==='production'){
+    process.env.MONGODB_URI = 'mongodb://rkn:123@ds137191.mlab.com:37191/todoapp';
 }

55141613793ab11429d581a701930554879ae4c7
rykun <rykunek@hotmail.com>
2017-08-09 23:45:06 +0200

    config test&dev envs

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env==='development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://rkn:123@ds137191.mlab.com:37191/todoapp';
+} else if(env==='test'){
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/toDoAppTest';
+    process.env.PORT = 3000;
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,5 +5,7 @@ mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true});
 
 //heroku
-mongoose.connect('mongodb://rkn:123@ds137191.mlab.com:37191/todoapp', { useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true});
+
+
 module.exports = { mongoose };

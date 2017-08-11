08c9e00d10961a2015eddaf1914ff93ceb5ee5a4
rykun <rykunek@hotmail.com>
2017-08-12 00:07:22 +0200

    auth middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const {ObjectID} =  require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+
+var { authenticate } =require('./middleware/authenticate');
+
+
 const port = process.env.PORT;
 
 var app=express();
@@ -109,6 +113,12 @@ app.post('/users',(req, res)=>{
     });
 });
 
+
+
+
+app.get('/users/me',authenticate, (req,res)=>{
+  res.send(req.user);
+});
 
 app.listen(port, ()=>{
   console.log(`Server started at port ${port}`);

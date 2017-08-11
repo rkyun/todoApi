d9f21e70a4d14bdbb4cc61d6c2e0932da3e5e0f7
rykun <rykunek@hotmail.com>
2017-08-11 23:25:37 +0200

    token method

--- server/server.js
+++ server/server.js
@@ -100,8 +100,10 @@ app.post('/users',(req, res)=>{
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body);
 
-  user.save().then((doc)=>{
-      res.status(200).send(doc);
+  user.save().then(()=>{
+   return user.generateAuthToken();
+    }).then((token)=>{
+      res.header('x-auth', token).send(user);
     }).catch((e)=>{
       res.status(400).send(e);
     });
